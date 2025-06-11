f1f2e5c0f1d267db32dea926662637ec52989602
Eddie Wu <edwardwu1234@gmail.com>
2025-06-11 18:32:52 -0400

    adding analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Wu-Viz/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    chunkSizeWarningLimit: 1600  // Suppress the chunk size warning
+  }
 })
